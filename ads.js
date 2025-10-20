a6c0020edc0142c60819d07b8fbaa17043e32cf0
Ivan <tylerfernandez140@gmail.com>
2025-10-21 00:40:31 +0800

    Add initial project files

---
+++ ads.js
@@ -0,0 +1,2 @@
+// This file is for optional future use for Adsterra/PropellerAds.
+// Ad code can be inserted here when needed.
---
+++ script.js
@@ -0,0 +1,3 @@
+function openTool(tool) {
+  window.location.href = `tools/${tool}.html`;
+}
